6e338c1bef9e1d4c5df317ae9bcb24b814a785a4
michuwili <wilmowicz@gmail.com>
2023-04-19 18:52:25 +0200

    commit message

--- Project.java
+++ Project.java
@@ -1,7 +1,7 @@
 public class Project {
     public static void main(String[] args) {
         System.out.println("Hello World GIT");
-        git
+        String n = "hello";
         int a = 5;
     }
 }
